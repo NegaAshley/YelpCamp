624d6336b674ebbbebadb44f4f87c8e51aecf50f
NegaAshley <ashleymxu@gmail.com>
2018-12-18 02:51:05 +0000

    Setup new campground POST route, add in body-parser, add basic unstyled form, add new campground to campgrounds array

--- README.md
+++ README.md
@@ -3,10 +3,16 @@
 * Add Landing Page
 * Add Campgrounds Page that lists all campgrounds
 
-Each Campground has:
+# Each Campground has:
 * Name
 * Image
 
 # Layout and Basic Styling
 * Create header and footer partials
 * Add in Bootrap
+
+# Creating New Campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var campgrounds = [
+        {name: "Johnny Appleseed Campground", image: "https://i.pinimg.com/originals/4e/92/6f/4e926ffff71d80b23d19f6e77b7ba81c.jpg"},
+        {name: "Bixler Lake", image: "https://50campfires.com/wp-content/uploads/2014/05/photo51-620x330.jpg"},    
+        {name: "Camp Potawotami", image: "https://photos.smugmug.com/photos/i-78dhF7q/0/M/i-78dhF7q-M.jpg"}    
+    ];
+    
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,13 +16,17 @@ app.get("/",  function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Johnny Appleseed Campground", image: "https://i.pinimg.com/originals/4e/92/6f/4e926ffff71d80b23d19f6e77b7ba81c.jpg"},
-        {name: "Bixler Lake", image: "https://50campfires.com/wp-content/uploads/2014/05/photo51-620x330.jpg"},    
-        {name: "Camp Potawotami", image: "https://photos.smugmug.com/photos/i-78dhF7q/0/M/i-78dhF7q-M.jpg"}    
-    ]
-    
     res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    // redirect back to campgrounds page
+    var newCampgroundName = req.body.newCampgroundName;
+    var newCampgroundImage = req.body.newCampgroundImage;
+    var newCampground = {name: newCampgroundName, image: newCampgroundImage};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
