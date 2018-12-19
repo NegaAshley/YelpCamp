3b012e9960f58ba8a07810b889b8ca5c06b8fda2
NegaAshley <ashleymxu@gmail.com>
2018-12-19 05:30:18 +0000

    Create schema and model for campground and add an example campground to the database

--- app.js
+++ app.js
@@ -10,10 +10,35 @@ var campgrounds = [
         {name: "Bixler Lake", image: "https://50campfires.com/wp-content/uploads/2014/05/photo51-620x330.jpg"},    
         {name: "Camp Potawotami", image: "https://photos.smugmug.com/photos/i-78dhF7q/0/M/i-78dhF7q-M.jpg"}    
     ];
-    
+
+mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+//Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+//Make model using campground schema
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//Create a campground
+Campground.create(
+    {
+        name: "Johnny Appleseed Campground", 
+        image: "https://i.pinimg.com/originals/4e/92/6f/4e926ffff71d80b23d19f6e77b7ba81c.jpg"
+    }, function(err, campground){
+        if(err){
+            console.log("Error creating campground!");
+            console.log(err);
+        }else{
+            console.log("We just saved a campground to the database!");
+            console.log(campground);
+        }
+    });
 
 app.get("/",  function(req, res){
     res.render("landing");
