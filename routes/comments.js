d57430d2daee12737384459808c9c1f5497c96eb
NegaAshley <ashleymxu@gmail.com>
2019-01-02 07:07:58 +0000

    Add content to edit comments page and  link to page

--- routes/comments.js
+++ routes/comments.js
@@ -45,8 +45,16 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //Edit - shows form to edit comment
-router.get("/:comments_id/edit", function(req, res){
-    res.send("Edit route for comment")
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            console.log("Error showing edit comment page!");
+            console.log(err);
+            res.redirect("back");
+        }else{
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment}); 
+        }
+    });
 });
 
 //Function to be used as middleware to check for auth
